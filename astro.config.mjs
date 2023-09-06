81f8ba94bf3f0cfb6cdcf8349e1090ae8c9e399b
Abdur Rehman Majeed <abdurrehman712@hotmail.com>
2023-09-06 12:28:07 +0500

    deno added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import alpinejs from "@astrojs/alpinejs";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  site: 'https://abdurrehman712.github.io/',
+  output: "server",
+  adapter: deno()
 });
